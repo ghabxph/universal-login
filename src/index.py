ef97b414d8c44d30f13e97fa5a9e1a5bf7457c36
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-01 21:09:56 +0800

    Update index.py
    Changes:
      - Implemented /setup/verify/mongodb_admin_power
      - Removed reference code (comment)

--- src/index.py
+++ src/index.py
@@ -132,71 +132,24 @@ def setup_verify_mongodbadminuser():
             "msg": "MongoDB username and/or password may be invalid. Unable to perform list_database_names operation."
         }), mimetype='application/json'), 403
 
-#
-# @app.route('/login', methods=['POST'])
-# def login():
-#     auto_create_default_user()
-#     user = db()['users'].find_one({"username": request.values.get('username').lower()})
-#     valid_username = request.values.get('username').lower() == user['username'].lower()
-#     valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
-#
-#     if valid_username and valid_password:
-#         token = jwt.JWT(header={'alg': 'HS256'}, claims={
-#             'usr': user['username'],
-#             'iat': int(time.time()),
-#             'exp': int(time.time() + 900)
-#         })
-#         token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
-#     return Response(json.dumps({"msg": "Invalid username and/or password"}), mimetype='application/json'), 403
-#
-#
-# @app.route('/login-agent', methods=['POST'])
-# def login_agent():
-#     auto_create_default_agent()
-#     query = {"name": request.values.get('name').lower()}
-#     agent = db()['agents'].find_one(query)
-#
-#     if agent and bcrypt.hashpw(request.values.get('password').encode('utf-8'), agent['password']) == agent['password']:
-#         new_password = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(32)).encode('utf-8')
-#         db()['agents'].update_one(query, {"$set": {"password": bcrypt.hashpw(new_password, bcrypt.gensalt())}})
-#         token = jwt.JWT(header={'alg': 'HS256'}, claims={
-#             'usr': agent['name'],
-#             'iat': int(time.time()),
-#             'exp': int(time.time() + 900)
-#         })
-#         token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response(json.dumps({"msg": "Agent connected successfully.", "jws": token.serialize(), "new_password": new_password.decode('utf-8')}), mimetype='application/json'), 200
-#     return Response(json.dumps({"msg": "Invalid agent credentials"}), mimetype='application/json'), 403
-#
-#
-# @app.route('/assets/agent.py', methods=['GET'])
-# def download_agent():
-#     with open('asset/agent.py', 'r') as agent_file:
-#         return Response(agent_file.read(), mimetype='application/octet-stream'), 200
-#
-#
-# @app.route('/verify', methods=['GET'])
-# def verify_token():
-#     try:
-#         jwt.JWT(jwt=request.headers.get('Authorization')[7:], key=jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response('{"msg":"Token is valid"}', mimetype='application/json'), 200
-#     except jws.InvalidJWSSignature:
-#         return Response('{"msg":"Token is invalid"}', mimetype='application/json'), 403
-#
-#
-# @app.errorhandler(404)
-# def error_404(e):
-#     return Response('{"msg": "Error 404. Resource not found."}', mimetype='application/json'), 404
-#
-#
-# def auto_create_default_user():
-#     default_user = db()['users'].find_one({"username": "ghabxph"})
-#     if not default_user:
-#         db()['users'].insert_one({"username": "ghabxph", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
-#
-#
-# def auto_create_default_agent():
-#     default_agent = db()['agents'].find_one({"name": "first-agent"})
-#     if not default_agent:
-#         db()['agents'].insert_one({"name": "first-agent", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
+
+@app.route('/setup/verify/mongodb_admin_power', methods=['POST'])
+def setup_verify_mongodbadminpower():
+    client = mongo_client(
+        host='svc-nosql',
+        serverSelectionTimeoutMS=500
+    )
+    try:
+        client.admin.authenticate(name=request.values.get('UL_DB_ROOT_USER'), password=request.values.get('UL_DB_ROOT_PASS'))
+        return Response(json.dumps({
+            "type": "success",
+            "msg": "rolesInfo command succeed. We assume that user has root priviledge.",
+            "rolesInfo": client.admin.command({
+                'rolesInfo': {'role': 'root', 'db': 'admin'},
+                'showPrivileges': True, 'showBuiltinRoles': True
+            })}), mimetype='application/json'), 200
+    except OperationFailure:
+        return Response(json.dumps({
+            "type": "success",
+            "msg": "rolesInfo command failed. We assume that user doesn't have root privilege."
+        }), mimetype='application/json'), 403
