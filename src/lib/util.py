072ea07f80d12b7b824602d74d8bf9c93bb213f6
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-02 14:07:22 +0800

    Update util.py

--- src/lib/util.py
+++ src/lib/util.py
@@ -9,11 +9,3 @@ def essential_env_present():
             os.environ.get('UL_TP_CHECK') is not None and \
             os.environ.get('UL_TP_URL') is not None and \
             os.environ.get('UL_TP_REQUEST_FORMAT') is not None
-
-
-def set_essential_env(essential_env):
-    for key in essential_env:
-        if essential_env[key] is None:
-            os.environ[key] = ''
-        else:
-            os.environ[key] = essential_env[key]
