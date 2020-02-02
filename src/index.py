8b0a9de75d1b29c1bde2757024e9431476335a3e
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-02 15:45:15 +0800

    Update index.py
    Changes:
      - Implemented /setup/get_environment route

--- src/index.py
+++ src/index.py
@@ -69,8 +69,8 @@ def set_environment():
     # Check all essential environment variable
     if essential_env_present():
 
-        # Do nothing..
-        return ''
+        # Redirects to /login page
+        return redirect('/login')
 
     essential_env = {
         "UL_DB_HOST": request.values.get('UL_DB_HOST'),
@@ -90,7 +90,18 @@ def set_environment():
             os.environ[key] = essential_env[key]
 
     # Set environment from submitted form
-    return 'Environment setup is done!'
+    with open('/html/setup-done.html', 'r') as html_file:
+
+        # index.html file
+        html = html_file.read()
+
+        # Check all essential environment variable
+        if essential_env_present():
+            # Perform re-setup.
+            return Response(html), 200
+
+        # Perform URL redirection
+        return redirect('/')
 
 
 @app.route('/setup/verify/mongodb', methods=['POST'])
@@ -172,85 +183,19 @@ def setup_verify_tpauth():
             "msg": "Connection error. Host either unresolvable, or cannot be reach."
         })), 403
 
-#
-#
-# @app.route('/test')
-# def test():
-#     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-#     username = 'ul_user_' + ''.join(random.choice(alphabet) for i in range(5))
-#     password = ''.join(random.choice(alphabet) for i in range(16))
-#     client = mongo_client(
-#         host='svc-nosql',
-#         username='root',
-#         password='root'
-#     )
-#     client.ul_login_db.command("createUser", username, pwd=password, roles=["readWrite"])
-#     client.admin.command("createUser", username, pwd=password, roles=["readWrite"])
-#     return Response('{"username":%s, "password":%s}' % (username, password), mimetype='application/json'), 200
-# @app.route('/login', methods=['POST'])
-# def login():
-#     auto_create_default_user()
-#     user = db()['users'].find_one({"username": request.values.get('username').lower()})
-#     valid_username = request.values.get('username').lower() == user['username'].lower()
-#     valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
-#
-#     if valid_username and valid_password:
-#         token = jwt.JWT(header={'alg': 'HS256'}, claims={
-#             'usr': user['username'],
-#             'iat': int(time.time()),
-#             'exp': int(time.time() + 900)
-#         })
-#         token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
-#     return Response(json.dumps({"msg": "Invalid username and/or password"}), mimetype='application/json'), 403
-#
-#
-# @app.route('/login-agent', methods=['POST'])
-# def login_agent():
-#     auto_create_default_agent()
-#     query = {"name": request.values.get('name').lower()}
-#     agent = db()['agents'].find_one(query)
-#
-#     if agent and bcrypt.hashpw(request.values.get('password').encode('utf-8'), agent['password']) == agent['password']:
-#         new_password = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(32)).encode('utf-8')
-#         db()['agents'].update_one(query, {"$set": {"password": bcrypt.hashpw(new_password, bcrypt.gensalt())}})
-#         token = jwt.JWT(header={'alg': 'HS256'}, claims={
-#             'usr': agent['name'],
-#             'iat': int(time.time()),
-#             'exp': int(time.time() + 900)
-#         })
-#         token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response(json.dumps({"msg": "Agent connected successfully.", "jws": token.serialize(), "new_password": new_password.decode('utf-8')}), mimetype='application/json'), 200
-#     return Response(json.dumps({"msg": "Invalid agent credentials"}), mimetype='application/json'), 403
-#
-#
-# @app.route('/assets/agent.py', methods=['GET'])
-# def download_agent():
-#     with open('asset/agent.py', 'r') as agent_file:
-#         return Response(agent_file.read(), mimetype='application/octet-stream'), 200
-#
-#
-# @app.route('/verify', methods=['GET'])
-# def verify_token():
-#     try:
-#         jwt.JWT(jwt=request.headers.get('Authorization')[7:], key=jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
-#         return Response('{"msg":"Token is valid"}', mimetype='application/json'), 200
-#     except jws.InvalidJWSSignature:
-#         return Response('{"msg":"Token is invalid"}', mimetype='application/json'), 403
-#
-#
-# @app.errorhandler(404)
-# def error_404(e):
-#     return Response('{"msg": "Error 404. Resource not found."}', mimetype='application/json'), 404
-#
-#
-# def auto_create_default_user():
-#     default_user = db()['users'].find_one({"username": "ghabxph"})
-#     if not default_user:
-#         db()['users'].insert_one({"username": "ghabxph", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
-#
-#
-# def auto_create_default_agent():
-#     default_agent = db()['agents'].find_one({"name": "first-agent"})
-#     if not default_agent:
-#         db()['agents'].insert_one({"name": "first-agent", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
+
+@app.route('/setup/get_environment', methods=['GET'])
+def setup_get_environment():
+    return Response(json.dumps({
+        "type": "success",
+        "msg": "Environment variable retrieval succeed. Please look at env variable.",
+        "env": {
+            "UL_DB_HOST": os.environ.get('UL_DB_HOST'),
+            "UL_DB_ROOT_USER": os.environ.get('UL_DB_ROOT_USER'),
+            "UL_DB_ROOT_PASS": os.environ.get('UL_DB_ROOT_PASS'),
+            "UL_DB_NAME_PREFIX": os.environ.get('UL_DB_NAME_PREFIX'),
+            "UL_TP_CHECK": os.environ.get('UL_TP_CHECK'),
+            "UL_TP_URL": os.environ.get('UL_TP_URL'),
+            "UL_TP_REQUEST_FORMAT": os.environ.get('UL_TP_REQUEST_FORMAT')
+        }
+    }), mimetype='application/json'), 200
