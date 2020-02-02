808e48ba8caf8b75df604397c84e842afc368d30
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-02 14:05:02 +0800

    Update util.py

--- src/lib/util.py
+++ src/lib/util.py
@@ -1,18 +1,14 @@
-from pymongo import MongoClient as db_client
 import os
 
 
 def essential_env_present():
     return os.environ.get('UL_DB_HOST') is not None and \
-            os.environ.get('UL_DB_USER') is not None and \
-            os.environ.get('UL_DB_PASS') is not None and \
-            os.environ.get('UL_DB_NAME') is not None and \
+            os.environ.get('UL_DB_ROOT_USER') is not None and \
+            os.environ.get('UL_DB_ROOT_PASS') is not None and \
+            os.environ.get('UL_DB_NAME_PREFIX') is not None and \
             os.environ.get('UL_TP_CHECK') is not None and \
             os.environ.get('UL_TP_URL') is not None and \
-            os.environ.get('UL_TP_METHOD') is not None and \
-            os.environ.get('UL_TP_USER_FIELD') is not None and \
-            os.environ.get('UL_TP_PASS_FIELD') is not None and \
-            os.environ.get('UL_TP_OTHER_FIELDS') is not None
+            os.environ.get('UL_TP_REQUEST_FORMAT') is not None
 
 
 def set_essential_env(essential_env):
@@ -21,15 +17,3 @@ def set_essential_env(essential_env):
             os.environ[key] = ''
         else:
             os.environ[key] = essential_env[key]
-
-
-def validate_submitted_env(essential_env):
-    client = db_client('mongodb://%s:%s@%s' % (
-        essential_env['UL_DB_HOST'],
-        essential_env['UL_DB_USER'],
-        essential_env['UL_DB_PASS']
-    ))
-    return {
-        'client': client.server_info(),
-        'valid': False
-    }
