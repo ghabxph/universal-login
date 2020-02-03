4e620d0045a74a8bcdfa2974e3b2fc42995f60f9
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-03 11:15:19 +0800

    Update index.py
    Note:
      - Under development, still have issues.

--- src/index.py
+++ src/index.py
@@ -4,12 +4,13 @@ from flask import request
 from flask import redirect
 from flask import jsonify
 from lib.util import essential_env_present
+from lib.util import random_str
 from pymongo import MongoClient as mongo_client
 from pymongo.errors import ServerSelectionTimeoutError
 from pymongo.errors import OperationFailure
 from requests.exceptions import ConnectionError
 from jwcrypto import jwt, jwk, jws
-# from lib.config import config
+from lib.mongo import db
 import os
 import json
 import random
@@ -20,6 +21,34 @@ import sys
 
 # Essential Instances
 app = Flask(__name__)
+
+
+@app.route('/test')
+def test():
+    key = jwk.JWK.generate(kty='EC', crv='P-256')
+    return jsonify({
+        'with_private': json.loads(key.export()),
+        'without_private': json.loads(key.export(private_key=False))
+    })
+
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+    user = db()['users'].find_one({"username": request.values.get('username').lower()})
+    valid_username = request.values.get('username').lower() == user['username'].lower()
+    valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
+
+    if valid_username and valid_password:
+
+        token = jwt.JWT(header={'alg': 'HS256'}, claims={
+            'usr': user['username'],
+            'iat': int(time.time()),
+            'exp': int(time.time() + 900)
+        })
+        # token.make_signed_token(os.environ.get('UL_KEY'))
+        token.make_signed_token(jwk.JWK(**json.loads(os.environ.get('UL_KEY'))))
+        return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
+    return Response(json.dumps({"msg": "Invalid username and/or password"}), mimetype='application/json'), 403
 
 
 @app.route('/assets')
@@ -96,11 +125,24 @@ def set_environment():
         password=os.environ.get('UL_DB_ROOT_PASS')
     )
 
+    # Assigns database name from prefix
+    os.environ['UL_DB_NAME'] = os.environ.get('UL_DB_NAME_PREFIX') + 'ul_db'
+
     # Creates an initial user: admin:admin
     # Note that this will have issue if we have third party authentication server.
     # I will make a separate setup for this, but for now, the implementation would be
     # just this simple. I will not handle third party auth for now...
-    client[os.environ.get('UL_DB_NAME_PREFIX') + 'ul_db'].users.insert_one({"username": "admin", "password": bcrypt.hashpw(b'admin', bcrypt.gensalt())})
+    client[os.environ.get('UL_DB_NAME')].users.insert_one({"username": "admin", "password": bcrypt.hashpw(b'admin', bcrypt.gensalt())})
+
+    # Assign a random username and password for universal login user account
+    os.environ['UL_DB_USER'] = 'ul_user_' + random_str(5)
+    os.environ['UL_DB_PASS'] = random_str(16)
+
+    # Creates a user dedicated for universal login only
+    client[os.environ.get('UL_DB_NAME')].command("createUser", os.environ.get('UL_DB_USER'), pwd=os.environ.get('UL_DB_PASS'), roles=["readWrite"])
+
+    # Generate a private key
+    os.environ['UL_KEY'] = jwk.JWK.generate(kty='RSA', size=2048).export()
 
     # Set environment from submitted form
     with open('/html/setup-done.html', 'r') as html_file:
@@ -213,10 +255,14 @@ def setup_get_environment():
         "type": "success",
         "msg": "Environment variable retrieval succeed. Please look at env variable.",
         "env": {
+            "UL_KEY": os.environ.get('UL_KEY'),
             "UL_DB_HOST": os.environ.get('UL_DB_HOST'),
             "UL_DB_ROOT_USER": os.environ.get('UL_DB_ROOT_USER'),
             "UL_DB_ROOT_PASS": os.environ.get('UL_DB_ROOT_PASS'),
             "UL_DB_NAME_PREFIX": os.environ.get('UL_DB_NAME_PREFIX'),
+            "UL_DB_USER": os.environ.get('UL_DB_USER'),
+            "UL_DB_PASS": os.environ.get('UL_DB_PASS'),
+            "UL_DB_NAME": os.environ.get('UL_DB_NAME'),
             "UL_TP_CHECK": os.environ.get('UL_TP_CHECK'),
             "UL_TP_URL": os.environ.get('UL_TP_URL'),
             "UL_TP_REQUEST_FORMAT": os.environ.get('UL_TP_REQUEST_FORMAT')
@@ -235,3 +281,68 @@ def render_login():
 
     with open('/html/login.html', 'r') as html:
         return html.read()
+#
+#
+# @app.route('/test')
+# def test():
+#     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+#     username = 'ul_user_' + ''.join(random.choice(alphabet) for i in range(5))
+#     password = ''.join(random.choice(alphabet) for i in range(16))
+#     client = mongo_client(
+#         host='svc-nosql',
+#         username='root',
+#         password='root'
+#     )
+#     client.ul_login_db.command("createUser", username, pwd=password, roles=["readWrite"])
+#     client.admin.command("createUser", username, pwd=password, roles=["readWrite"])
+#     return Response('{"username":%s, "password":%s}' % (username, password), mimetype='application/json'), 200
+#
+# @app.route('/login-agent', methods=['POST'])
+# def login_agent():
+#     auto_create_default_agent()
+#     query = {"name": request.values.get('name').lower()}
+#     agent = db()['agents'].find_one(query)
+#
+#     if agent and bcrypt.hashpw(request.values.get('password').encode('utf-8'), agent['password']) == agent['password']:
+#         new_password = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') for i in range(32)).encode('utf-8')
+#         db()['agents'].update_one(query, {"$set": {"password": bcrypt.hashpw(new_password, bcrypt.gensalt())}})
+#         token = jwt.JWT(header={'alg': 'HS256'}, claims={
+#             'usr': agent['name'],
+#             'iat': int(time.time()),
+#             'exp': int(time.time() + 900)
+#         })
+#         token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
+#         return Response(json.dumps({"msg": "Agent connected successfully.", "jws": token.serialize(), "new_password": new_password.decode('utf-8')}), mimetype='application/json'), 200
+#     return Response(json.dumps({"msg": "Invalid agent credentials"}), mimetype='application/json'), 403
+#
+#
+# @app.route('/assets/agent.py', methods=['GET'])
+# def download_agent():
+#     with open('asset/agent.py', 'r') as agent_file:
+#         return Response(agent_file.read(), mimetype='application/octet-stream'), 200
+#
+#
+# @app.route('/verify', methods=['GET'])
+# def verify_token():
+#     try:
+#         jwt.JWT(jwt=request.headers.get('Authorization')[7:], key=jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
+#         return Response('{"msg":"Token is valid"}', mimetype='application/json'), 200
+#     except jws.InvalidJWSSignature:
+#         return Response('{"msg":"Token is invalid"}', mimetype='application/json'), 403
+#
+#
+# @app.errorhandler(404)
+# def error_404(e):
+#     return Response('{"msg": "Error 404. Resource not found."}', mimetype='application/json'), 404
+#
+#
+# def auto_create_default_user():
+#     default_user = db()['users'].find_one({"username": "ghabxph"})
+#     if not default_user:
+#         db()['users'].insert_one({"username": "ghabxph", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
+#
+#
+# def auto_create_default_agent():
+#     default_agent = db()['agents'].find_one({"name": "first-agent"})
+#     if not default_agent:
+#         db()['agents'].insert_one({"name": "first-agent", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
