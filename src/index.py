0a4b2b26025d553f2f7f1d93e08038be290cf8af
ghabxph <11975597+ghabxph@users.noreply.github.com>
2020-02-03 13:37:37 +0800

    Update index.js
    Changes:
      - Login is now minimally working
    
    TODO:
      - I need to be able to use third party as source for auth.

--- src/index.py
+++ src/index.py
@@ -35,19 +35,21 @@ def test():
 @app.route('/api/v1/login', methods=['POST'])
 def login():
     user = db()['users'].find_one({"username": request.values.get('username').lower()})
-    valid_username = request.values.get('username').lower() == user['username'].lower()
-    valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
 
-    if valid_username and valid_password:
+    if user is not None:
+        valid_username = request.values.get('username').lower() == user['username'].lower()
+        valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
 
-        token = jwt.JWT(header={'alg': 'HS256'}, claims={
-            'usr': user['username'],
-            'iat': int(time.time()),
-            'exp': int(time.time() + 900)
-        })
-        # token.make_signed_token(os.environ.get('UL_KEY'))
-        token.make_signed_token(jwk.JWK(**json.loads(os.environ.get('UL_KEY'))))
-        return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
+        if valid_username and valid_password:
+
+            token = jwt.JWT(header={'alg': 'HS256'}, claims={
+                'usr': user['username'],
+                'iat': int(time.time()),
+                'exp': int(time.time() + 900)
+            })
+            # token.make_signed_token(os.environ.get('UL_KEY'))
+            token.make_signed_token(jwk.JWK(**json.loads(os.environ.get('UL_KEY'))))
+            return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
     return Response(json.dumps({"msg": "Invalid username and/or password"}), mimetype='application/json'), 403
 
 
