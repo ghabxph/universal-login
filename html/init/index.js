1f8c145eedecacc2937b0f4e90126612ea9a916e
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-09 17:14:11 +0800

    Update index.js

--- html/init/index.js
+++ html/init/index.js
@@ -1,3 +1,80 @@
 $(() => {
-    // Do something...
+    $('.panel.controller li').click((el) => {
+
+        // Remove all 'selected' class to li element
+        $('.panel.controller li').removeClass('selected');
+
+        // Add 'selected' class to clicked item (to perceive that we switched panel)
+        $(el.target).addClass('selected');
+
+        // Hide all panel.body
+        $('div.panel.body').attr('hidden', '');
+
+        // Show target panel.body
+        $('div[data-panel-name="' + $(el.target).attr('data-for') + '"].panel.body').removeAttr('hidden');
+    });
+
+    $('.panel.body a').click((el) => {
+
+        // Prevent showing '#'
+        el.preventDefault();
+
+        // Run function based on link's text
+        switch($(el.target).text()) {
+            case 'Show token':
+                show_token();
+                break;
+            case 'Generate new token':
+                generate_new_token();
+                break;
+        }
+    });
+
+    $('.panel.body[data-panel-name="manage-other-instance"] form').submit((el) => {
+
+        // Prevent form from submitting.
+        el.preventDefault();
+
+        // Dddd
+        $.post('/api/v1/manage_other_instance', {
+            'server_url': $(el.target).find('input[name="server_url"]').val(),
+            'secret_admin_key': $(el.target).find('input[name="secret_admin_key"]').val()
+        }, (res) => {
+
+            // Shows server response to console (temporary)
+            console.log(res)
+        }).fail((xhr) => {
+
+            // Prints error to console
+            console.log(xhr.responseText);
+        })
+    });
+
+    function show_token() {
+        $.ajax({
+            url: '/api/v1/admin/token',
+            type: 'GET',
+            success: (res) => {
+                $('.panel.body .admin-token').text(res);
+                $('.panel.body .server-msg').text(res.msg);
+            }
+        }).fail((xhr) => {
+            $('.panel.body .admin-token').text(xhr.responseText);
+            $('.panel.body .server-msg').text(res.msg);
+        });
+    }
+
+    function generate_new_token() {
+        $.ajax({
+            url: '/api/v1/admin/token',
+            type: 'PUT',
+            success: (res) => {
+                $('.panel.body .admin-token').text(res);
+                $('.panel.body .server-msg').text(res.msg);
+            }
+        }).fail((xhr) => {
+            $('.panel.body .admin-token').text(xhr.responseText);
+            $('.panel.body .server-msg').text(xhr.msg);
+        });
+    }
 });
