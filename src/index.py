13288f5c8721e5206213d9dd376768d0cc8179c1
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-09 17:14:27 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -11,7 +11,10 @@ from pymongo.errors import OperationFailure
 from requests.exceptions import ConnectionError
 from jwcrypto import jwt, jwk, jws
 from lib.mongo import db
+from controllers.api.v1.login_controller import LoginController
+from controllers.assets_controller import AssetsController
 from controllers.init_controller import InitController
+from controllers.mongo_setup_controller import MongoSetupController
 import os
 import json
 import random
@@ -35,197 +38,38 @@ def test():
 
 @app.route('/api/v1/login', methods=['POST'])
 def login():
-    user = db()['users'].find_one({"username": request.values.get('username').lower()})
-
-    if user is not None:
-        valid_username = request.values.get('username').lower() == user['username'].lower()
-        valid_password = bcrypt.hashpw(request.values.get('password').encode('utf-8'), user['password']) == user['password']
-
-        if valid_username and valid_password:
-
-            token = jwt.JWT(header={'alg': 'HS256'}, claims={
-                'usr': user['username'],
-                'iat': int(time.time()),
-                'exp': int(time.time() + 900)
-            })
-            # token.make_signed_token(os.environ.get('UL_KEY'))
-            token.make_signed_token(jwk.JWK(**json.loads(os.environ.get('UL_KEY'))))
-            return Response(json.dumps({"msg": "Login success.", "jws": token.serialize()}), mimetype='application/json'), 200
-    return Response(json.dumps({"msg": "Invalid username and/or password"}), mimetype='application/json'), 403
+    return LoginController.post()
 
 
 @app.route('/assets')
 def assets():
-    asset = '/html/%s' % request.values.get('file')
-
-    # Basic security check
-    if os.path.commonprefix((os.path.realpath(asset), '/html/')) != '/html/':
-        return Response('Asset not found.', mimetype='text/plain'), 404
-
-    try:
-        with open(asset) as asset_file:
-
-            # Returns JS
-            if asset.endswith('.js'):
-                return Response(asset_file.read(), mimetype='application/javascript'), 200
-
-            # Returns CSS
-            if asset.endswith('.css'):
-                return Response(asset_file.read(), mimetype='text/css'), 200
-
-        # Asset not found
-        return Response('Asset not found.', mimetype='text/plain'), 404
-
-    # Returns 404 if file does not exist.
-    except FileNotFoundError:
-
-        # Asset not found
-        return Response('Asset not found.', mimetype='text/plain'), 404
+    return AssetsController.get()
 
 
 @app.route('/', methods=['GET'])
 def setup():
-
-    with open('/html/setup.html', 'r') as html_file:
-
-        # index.html file
-        html = html_file.read()
-
-        # Check all essential environment variable
-        if essential_env_present():
-
-            # Perform URL redirection (to /login)
-            return redirect('/login')
-
-        # Perform re-setup.
-        return Response(html), 200
+    return MongoSetupController.get()
 
 
 # @status: Under development
 @app.route('/', methods=['POST'])
 def set_environment():
-
-    # Check all essential environment variable
-    if essential_env_present():
-
-        # Redirects to /login page
-        return redirect('/login')
-
-    essential_env = {
-        "UL_DB_HOST": request.values.get('UL_DB_HOST'),
-        "UL_DB_ROOT_USER": request.values.get('UL_DB_ROOT_USER'),
-        "UL_DB_ROOT_PASS": request.values.get('UL_DB_ROOT_PASS'),
-        "UL_DB_NAME_PREFIX": request.values.get('UL_DB_NAME_PREFIX'),
-        "UL_TP_CHECK": request.values.get('UL_TP_CHECK'),
-        "UL_TP_URL": request.values.get('UL_TP_URL'),
-        "UL_TP_REQUEST_FORMAT": request.values.get('UL_TP_REQUEST_FORMAT')
-    }
-
-    # Sets essential env setting
-    for key in essential_env:
-        if essential_env[key] is None:
-            os.environ[key] = ''
-        else:
-            os.environ[key] = essential_env[key]
-
-    # Create a client instance (mongodb)
-    client = mongo_client(
-        host=os.environ.get('UL_DB_HOST'),
-        username=os.environ.get('UL_DB_ROOT_USER'),
-        password=os.environ.get('UL_DB_ROOT_PASS')
-    )
-
-    # Assigns database name from prefix
-    os.environ['UL_DB_NAME'] = os.environ.get('UL_DB_NAME_PREFIX') + 'ul_db'
-
-    # Creates an initial user: admin:admin
-    # Note that this will have issue if we have third party authentication server.
-    # I will make a separate setup for this, but for now, the implementation would be
-    # just this simple. I will not handle third party auth for now...
-    client[os.environ.get('UL_DB_NAME')].users.insert_one({"username": "admin", "password": bcrypt.hashpw(b'admin', bcrypt.gensalt())})
-
-    # Assign a random username and password for universal login user account
-    os.environ['UL_DB_USER'] = 'ul_user_' + random_str(5)
-    os.environ['UL_DB_PASS'] = random_str(16)
-
-    # Creates a user dedicated for universal login only
-    client[os.environ.get('UL_DB_NAME')].command("createUser", os.environ.get('UL_DB_USER'), pwd=os.environ.get('UL_DB_PASS'), roles=["readWrite"])
-
-    # Generate a symmetric key
-    # Note: I wish to use asymmetric key, but I have issues with EC or RSA.
-    # Errors: TypeError: object of type '_RSAPrivateKey' has no len() or
-    #         TypeError: object of type '_EllipticCurvePrivateKey' has no len()
-    os.environ['UL_KEY'] = jwk.JWK.generate(kty='oct', size=256).export()
-
-    # Set environment from submitted form
-    with open('/html/setup-done.html', 'r') as html_file:
-
-        # index.html file
-        html = html_file.read()
-
-        # Check all essential environment variable
-        if essential_env_present():
-            # Perform re-setup.
-            return Response(html), 200
-
-        # Perform URL redirection
-        return redirect('/')
+    return MongoSetupController.set_environment()
 
 
 @app.route('/setup/verify/mongodb', methods=['POST'])
 def setup_verify_mongodb():
-    client = mongo_client(host=request.values.get('UL_DB_HOST'), serverSelectionTimeoutMS=5000)
-    try:
-        return Response(json.dumps({
-            "type": "success",
-            "msg": "Connection to mongodb host is successful.",
-            "server_info": client.server_info()
-        }, indent=4), mimetype='application/json'), 200
-    except ServerSelectionTimeoutError:
-        return Response('{"type":"error", "msg":"Unable to connect to mongodb host. Are you sure that the hostname is reachable?"}', mimetype='application/json'), 403
+    return MongoSetupController.setup_verify_mongodb()
 
 
 @app.route('/setup/verify/mongodb_admin_user', methods=['POST'])
 def setup_verify_mongodbadminuser():
-    client = mongo_client(
-        host=request.values.get('UL_DB_HOST'),
-        username=request.values.get('UL_DB_ROOT_USER'),
-        password=request.values.get('UL_DB_ROOT_PASS'),
-        serverSelectionTimeoutMS=500
-    )
-    try:
-        return Response(json.dumps({
-            "type": "success",
-            "msg": "Test list_database operation is successful.",
-            "list_databases": client.list_database_names()
-        }), mimetype="application/json"), 200
-    except OperationFailure:
-        return Response(json.dumps({
-            "type": "error",
-            "msg": "MongoDB username and/or password may be invalid. Unable to perform list_database_names operation."
-        }), mimetype='application/json'), 403
+    return MongoSetupController.setup_verify_mongodbadminuser()
 
 
 @app.route('/setup/verify/mongodb_admin_power', methods=['POST'])
 def setup_verify_mongodbadminpower():
-    client = mongo_client(
-        host='svc-nosql',
-        serverSelectionTimeoutMS=500
-    )
-    try:
-        client.admin.authenticate(name=request.values.get('UL_DB_ROOT_USER'), password=request.values.get('UL_DB_ROOT_PASS'))
-        return Response(json.dumps({
-            "type": "success",
-            "msg": "rolesInfo command succeed. We assume that user has root priviledge.",
-            "rolesInfo": client.admin.command({
-                'rolesInfo': {'role': 'root', 'db': 'admin'},
-                'showPrivileges': True, 'showBuiltinRoles': True
-            })}), mimetype='application/json'), 200
-    except OperationFailure:
-        return Response(json.dumps({
-            "type": "success",
-            "msg": "rolesInfo command failed. We assume that user doesn't have root privilege."
-        }), mimetype='application/json'), 403
+    return MongoSetupController.setup_verify_mongodbadminpower()
 
 
 @app.route('/setup/verify/third_party_auth', methods=['POST'])
