44f58faae6f6675a7baaa0f7c979c6179539432f
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-02-17 00:33:19 +0800

    Update mongo_setup_controller.py
    Changes:
      - set_environment() is deprecated and to be removed some time later
      - Everything below the comment "Everything below is deprecated" is now
        deprecated and to be removed some time.
      - Validation is being done on set_environment_new() (previously
        set_environment) instead of per call from what I have implemented
        before.

--- src/controllers/mongo_setup_controller.py
+++ src/controllers/mongo_setup_controller.py
@@ -31,6 +31,10 @@ class MongoSetupController:
 
     @staticmethod
     def set_environment():
+        """
+        Deprecated
+        :return:
+        """
 
         # Check all essential environment variable
         if essential_env_present():
@@ -99,6 +103,160 @@ class MongoSetupController:
             # Perform URL redirection
             return redirect('/')
 
+    @staticmethod
+    def set_environment_new():
+
+        # Check all essential environment variable
+        if essential_env_present():
+
+            # Returns an access denied response
+            return Response(json.dumps({
+                "type": "error",
+                "msg": "Access is denied."
+            }), mimetype='application/json'), 403
+
+        essential_env = {
+            "UL_DB_HOST": request.values.get('UL_DB_HOST'),
+            "UL_DB_ROOT_USER": request.values.get('UL_DB_ROOT_USER'),
+            "UL_DB_ROOT_PASS": request.values.get('UL_DB_ROOT_PASS'),
+            "UL_DB_NAME_PREFIX": request.values.get('UL_DB_NAME_PREFIX'),
+            "UL_TP_CHECK": request.values.get('UL_TP_CHECK'),
+            "UL_TP_URL": request.values.get('UL_TP_URL'),
+            "UL_TP_REQUEST_FORMAT": request.values.get('UL_TP_REQUEST_FORMAT')
+        }
+
+        # Perform validation
+        status = MongoSetupController.validate()
+        if status.get('type') == 'error':
+            return Response(json.dumps(status), mimetype='application/json'), 403
+
+        # Sets essential env setting
+        for key in essential_env:
+            if essential_env[key] is None:
+                config(key, '')
+            else:
+                config(key, essential_env[key])
+
+        # Create a client instance (mongodb)
+        client = mongo_client(
+            host=config('UL_DB_HOST'),
+            username=config('UL_DB_ROOT_USER'),
+            password=config('UL_DB_ROOT_PASS')
+        )
+
+        # Assigns database name from prefix
+        config('UL_DB_NAME', config('UL_DB_NAME_PREFIX') + 'ul_db')
+
+        # Creates an initial user: admin:admin
+        # Note that this will have issue if we have third party authentication server.
+        # I will make a separate setup for this, but for now, the implementation would be
+        # just this simple. I will not handle third party auth for now...
+        client[config('UL_DB_NAME')].users.insert_one(
+            {"username": "admin", "password": bcrypt.hashpw(b'admin', bcrypt.gensalt())})
+
+        # Assign a random username and password for universal login user account
+        config('UL_DB_USER', 'ul_user_' + random_str(5))
+        config('UL_DB_PASS', random_str(16))
+
+        # Creates a user dedicated for universal login only
+        client[config('UL_DB_NAME')].command("createUser", config('UL_DB_USER'),
+                                                     pwd=config('UL_DB_PASS'), roles=["readWrite"])
+
+        # Generate a symmetric key
+        # Note: I wish to use asymmetric key, but I have issues with EC or RSA.
+        # Errors: TypeError: object of type '_RSAPrivateKey' has no len() or
+        #         TypeError: object of type '_EllipticCurvePrivateKey' has no len()
+        config('UL_KEY', jwk.JWK.generate(kty='oct', size=256).export())
+
+        return Response(json.dumps({
+            "type": "success",
+            "msg": "Environment has been successfuly set."
+        }), mimetype='application/json'), 200
+
+    @staticmethod
+    def validate():
+        try:
+            MongoSetupController.validate_mongo_host()
+            MongoSetupController.validate_mongo_admin()
+            MongoSetupController.validate_mongo_power()
+            MongoSetupController.validate_tpauth()
+            return {
+                "type": "success",
+                "msg": "No error."
+            }
+        except Exception as e:
+            return json.loads(str(e))
+
+    @staticmethod
+    def validate_mongo_host():
+        client = mongo_client(host=request.values.get('UL_DB_HOST'), serverSelectionTimeoutMS=5000)
+        try:
+            client.server_info()
+        except ServerSelectionTimeoutError:
+            raise Exception(json.dumps({
+                "type": "error",
+                "msg": "Unable to connect to mongodb host. Are you sure that the hostname is reachable?"
+            }))
+
+    @staticmethod
+    def validate_mongo_admin():
+        client = mongo_client(
+            host=request.values.get('UL_DB_HOST'),
+            username=request.values.get('UL_DB_ROOT_USER'),
+            password=request.values.get('UL_DB_ROOT_PASS'),
+            serverSelectionTimeoutMS=500
+        )
+        try:
+            client.list_database_names()
+        except OperationFailure:
+            raise Exception(json.dumps({
+                "type": "error",
+                "msg": "MongoDB username and/or password may be invalid. Unable to perform list_database_names operation."
+            }))
+
+    @staticmethod
+    def validate_mongo_power():
+        client = mongo_client(
+            host='svc-nosql',
+            serverSelectionTimeoutMS=500
+        )
+        try:
+            client.admin.authenticate(name=request.values.get('UL_DB_ROOT_USER'),
+                                      password=request.values.get('UL_DB_ROOT_PASS'))
+            client.admin.command({
+                'rolesInfo': {'role': 'root', 'db': 'admin'},
+                'showPrivileges': True, 'showBuiltinRoles': True
+            })
+        except OperationFailure:
+            raise Exception(json.dumps({
+                "type": "success",
+                "msg": "rolesInfo command failed. We assume that user doesn't have root privilege."
+            }))
+
+    @staticmethod
+    def validate_tpauth():
+        if request.values.get('UL_TP_CHECK') == 'false' or request.values.get('UL_TP_CHECK') is None:
+            return
+
+        if request.values.get('UL_TP_REQUEST_FORMAT') is None or request.values.get('UL_TP_REQUEST_FORMAT') == '':
+            data = {
+                'username': '$username',
+                'password': '$password'
+            }
+        else:
+            data = json.loads(request.values.get('UL_TP_REQUEST_FORMAT'))
+
+        try:
+            requests.post(request.values.get('UL_TP_URL'), data=data)
+        except:
+            raise Exception(json.dumps({
+                "type": "error",
+                "msg": "Connection error. Host either unresolvable, or cannot be reach."
+            }))
+
+    """
+    Everything below is deprecated ~
+    """
     @staticmethod
     def setup_verify_mongodb():
         client = mongo_client(host=request.values.get('UL_DB_HOST'), serverSelectionTimeoutMS=5000)
